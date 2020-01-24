ca390c989f3b15183860647fcac4b8f0e1d5f7ef
FrankZou21 <frankzou2000@hotmail.com>
2020-01-24 12:45:54 -0800

    adding screenshots

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,6 +4,13 @@ import "./styles/Projects.css"
 
 const projects = [
   {
+    name: "Personal Website",
+    background: "https://images.pexels.com/photos/374006/pexels-photo-374006.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    description: "A personnal website created entirely in React",
+    image: ["https://camo.githubusercontent.com/6e68e406606779aba6ea96f510e3653d857af8d4/68747470733a2f2f6d656469612e67697068792e636f6d2f6d656469612f676b51517039314271676346516758466e6c2f67697068792e676966", "https://camo.githubusercontent.com/c31f4cbb872cfe7bec388a0569aac0ebb90e85df/68747470733a2f2f6d656469612e67697068792e636f6d2f6d656469612f4a35534e55347a4333334e6c4239655266462f67697068792e676966", "https://camo.githubusercontent.com/3896d36bb2607a55acb9f67829361314c2a14099/68747470733a2f2f6d656469612e67697068792e636f6d2f6d656469612f6c6d755675755254754836393353326d43302f67697068792e676966", "https://camo.githubusercontent.com/1a8191943965c8f80417d8e44b7c1f5acf409991/68747470733a2f2f6d656469612e67697068792e636f6d2f6d656469612f596b7a4a4754704c4e4c41717a354377634a2f67697068792e676966", "https://camo.githubusercontent.com/ef0e00533b492c1e1dcf571559e51babb5e1eb9f/68747470733a2f2f6d656469612e67697068792e636f6d2f6d656469612f564a726578414c48636376766d58454871342f67697068792e676966", "https://camo.githubusercontent.com/bc9e20975e2bb8c5f5611c60f7d607fcade99cab/68747470733a2f2f6d656469612e67697068792e636f6d2f6d656469612f6c4f616b777a564f49396471783833306f4a2f67697068792e676966"],
+    link: "https://github.com/FrankZou21/mywebsite"
+  },
+  {
     name: "Trekk",
     background: "https://www.augment.com/blog/wp-content/uploads/2017/04/apple-augmented-reality2-100709335-large.jpg",
     description: "Trekk is a mobile application that allows users to create and share custom augmented reality tours, or 'Trekks'. A user is able to customize their own Trekks by submitting marker images with their corresponding media to be displayed. These media can be in the form of a text description, image url and/or video url. Guests can access private Trekks via a randomly generated access code provided by the Trekk owner.",
